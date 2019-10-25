74b138c459fa5b393a682e1d17abaf3a4fb1508c
Iwan Evans <iwangevans@gmail.com>
2019-10-25 17:39:06 +0100

    Initial Commit

---
+++ Shopping List Core Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping List Core Data
+//
+//  Created by IWAN EVANS on 25/10/2019.
+//  Copyright © 2019 Iwan Evans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
